c3c636673de048a6da94a58788030d47c6acda19
holyshared <holy.shared.design@gmail.com>
2016-09-22 18:02:31 +0900

    add current test name

--- src/report.rs
+++ src/report.rs
@@ -25,3 +25,21 @@ pub fn records_from_file(file: &str) -> Result<Vec<LCOVRecord>, ParseError> {
     let parser = try!(LCOVParser::from_file(file));
     Ok(try!(parser.parse()))
 }
+
+struct ReportParser {
+    test_name: Option<String>
+}
+
+impl ReportParser {
+    fn parse(&mut self, file: &str) -> Result<(), ParseError> {
+        let records = try!(records_from_file(file));
+
+        for record in records.iter() {
+            match record {
+                &LCOVRecord::TestName(ref name) => self.test_name = name.clone(),
+                _ => { continue; }
+            };
+        }
+        Ok(())
+    }
+}
