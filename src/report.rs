953b5f0be872b5edb169fe66ff8173d68781f991
holyshared <holy.shared.design@gmail.com>
2016-09-25 17:06:22 +0900

    parse data record

--- src/report.rs
+++ src/report.rs
@@ -55,17 +55,21 @@ impl Default for Test {
 // my $sumbrcount
 // my $sumfnccount
 struct SumCount {
-    sum_count: HashMap<i32, i32>, // key: line number, value: count
-    sum_br_count: HashMap<String, i32>, // key: function name, value: execution count
-    sum_fn_count: i32 // FIXME br data structure
+    sum_count: HashMap<u32, u32>, // key: line number, value: count
+    sum_br_count: HashMap<String, u32>, // key: function name, value: execution count
+    sum_fn_count: u32 // FIXME br data structure
 }
+
+// key: line_number, value: checksum value
+type CheckSum = HashMap<u32, String>;
 
 struct ReportParser {
     test_name: Option<String>,
     source_name: Option<String>,
     test: Option<Test>,
     tests: HashMap<String, Test>,
-    sum: SumCount
+    sum: SumCount,
+    checksum: CheckSum
 }
 
 impl ReportParser {
@@ -76,6 +80,11 @@ impl ReportParser {
             match record {
                 &LCOVRecord::TestName(ref name) => self.on_test_name(name),
                 &LCOVRecord::SourceFile(ref name) => self.on_source_file(name),
+                &LCOVRecord::Data(ref line_number, ref execution_count, ref checksum) => self.on_data(
+                    line_number,
+                    execution_count,
+                    checksum,
+                ),
                 _ => { continue; }
             };
         }
@@ -89,6 +98,9 @@ impl ReportParser {
 
         match self.test_name {
             Some(ref current_test_name) => {
+                // $testcount = $testdata->{$testname};
+                // $testfnccount = $testfncdata->{$testname};
+                // $testbrcount = $testbrdata->{$testname};
                 if !self.tests.contains_key(current_test_name) {
                     self.tests.insert(current_test_name.to_string(), Test::default());
                 }
@@ -96,6 +108,34 @@ impl ReportParser {
                 self.test = Some(test.unwrap().clone());
             },
             None => self.test = Some(Test::default())
+        }
+    }
+    fn on_data(&mut self, line_number: &u32, execution_count: &u32, checksum: &Option<String>) {
+        let mut sum_count = self.sum.sum_count.entry(line_number.clone()).or_insert(0);
+        *sum_count += *execution_count;
+
+        if self.test_name.is_some() {
+            if self.test.is_some() {
+                let mut test = self.test.as_mut().unwrap();
+                let mut test_count = test.test_count.entry(line_number.clone()).or_insert(0);
+                *test_count += *execution_count;
+            }
+        }
+
+        if checksum.is_none() {
+            return;
+        }
+
+        if !self.checksum.contains_key(line_number) {
+            let checksum_value = checksum.clone().unwrap();
+            self.checksum.insert(line_number.clone(), checksum_value);
+            return;
+        }
+
+        let checksum_value = checksum.clone().unwrap();
+        let current_checksum = self.checksum.get(line_number).unwrap();
+        if current_checksum != &checksum_value {
+            println!("{} {}", current_checksum, checksum_value);
         }
     }
 }
